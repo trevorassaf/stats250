0fcda5bf843158edd2da808b07b716ccebc93f1d
Trevor Assaf <astrev@umich.edu>
2015-05-09 02:43:52 -0700

    hw1

---
+++ hw/hw1/q6/hw1q6.r
@@ -0,0 +1,20 @@
+#! /usr/bin/Rscript
+
+# Constants
+LAB1_END_IDX <- 26
+
+# Main
+args <- commandArgs(trailingOnly = TRUE)
+load(args[1])
+exam1 <- Exam1scores['exam1'][[1]]
+lab1 <- exam1[1:26]
+lab2 <- exam1[27:53]
+box_plots <- boxplot(
+                     lab1,
+                     lab2,
+                     main='Exam 1 Score Distributions For Labs 1 and 2')
+
+# z <- c("lab1", "lab2")
+# dataList <- lapply(z, get, envir=environment())
+# names(dataList) <- z
+# box_plots <- boxplot(dataList)
