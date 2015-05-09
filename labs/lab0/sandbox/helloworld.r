28d02f47d91c298aa3ff830b4b50d8b6b72838de
Trevor Assaf <astrev@umich.edu>
2015-05-08 17:33:30 -0700

    stats 250 commit 1

---
+++ labs/lab0/sandbox/helloworld.r
@@ -0,0 +1,13 @@
+#! /usr/bin/env Rscript
+
+# print hello world message 
+sink("helloworld_routput.txt")
+cat("Hello world!\n")
+
+# print 100 random 1/0 values
+# x <- rnorm(100)
+# print(x)
+# sink()
+# png("helloworld_rplot.png")
+# hist(x)
+# dev.off()
---
+++ labs/lab1/prelab/prelab1.r
@@ -0,0 +1,12 @@
+#! /usr/bin/Rscript
+
+args <- commandArgs(trailingOnly = TRUE)
+data_file = args[1]
+print(data_file)
+load(data_file)
+study_hours <- studyhabits["StudyHrs"]
+study_hours <- study_hours[[1]]
+study_hours_hist <- hist(x=study_hours)
+png(filename="study_hours_hist.png")
+plot(study_hours_hist, labels=TRUE, main="Hours per week students spent studying -- Trevor Assaf", xlab="Hours", ylab="Number of Students")
+dev.off()
