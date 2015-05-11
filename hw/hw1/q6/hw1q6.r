821bb1f70326fae0bf1361beedfe75a40e5c68dc
Trevor Assaf <astrev@umich.edu>
2015-05-11 12:03:40 -0700

    dont quite remember

--- hw/hw1/q6/hw1q6.r
+++ hw/hw1/q6/hw1q6.r
@@ -9,6 +9,8 @@ load(args[1])
 exam1 <- Exam1scores['exam1'][[1]]
 lab1 <- exam1[1:26]
 lab2 <- exam1[27:53]
+lab1
+lab2
 box_plots <- boxplot(
                      lab1,
                      lab2,
